2a788a411a06ae935122f5e13e7d392f9284e4f5
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-04-19 13:59:21 +0530

    sorting

---
+++ binarysearch.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int starting = 0;
+    int ending = n;
+
+    while(starting <= ending) {
+        int mid = (starting + ending)/2;
+
+        if(arr[mid] == key) {
+            return mid;
+        }
+        else if(arr[mid] > key) {
+            ending = mid - 1;
+        }
+        else {
+            starting = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+
+    int key;
+    cin>>key;
+
+    cout<<binarySearch(arr, n, key)<<endl;
+
+    return 0;
+}
+
