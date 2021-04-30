c8d7ae0b1ea0c5a30be648657d475df04baf643c
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-04-30 15:23:32 +0530

    array

---
+++ CharacterArray.cpp
@@ -0,0 +1,16 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    char arr[100];
+    cin>>arr;
+    cout<<arr;
+    cout<<arr[2];
+    int i=0;
+    while(arr[i] != '\0') {
+        cout<<arr[i]<<endl;
+        i++
+    }
+
+    return 0;
+}
