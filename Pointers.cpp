9202b7ec7f2d03daec6072e193db9268f254977d
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-05-12 22:23:37 +0530

    sort

--- Pointers.cpp
+++ Pointers.cpp
@@ -1,13 +1,36 @@
 #include<iostream>
 using namespace std;
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
+    /*
     int a=10;
     int *aptr = &a;
 
     cout<<*aptr<<endl;
     *aptr=20;
     cout<<a<<endl;
+
+    int arr[] = {10,20,30};
+    cout<<arr<<endl;
+
+    int *ptr = arr;
+    for(int i=0; i<3; i++) {
+        cout<<*ptr<<endl;
+        ptr++;
+    }
+    */
+
+    int a=2;
+    int b=4;
+
+    swap(&a, &b);
+    cout<<a<<" "<<b<<endl;
 
     return 0;
 }
