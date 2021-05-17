0f17d0cbcc4d3a65bd66437f5d6346e1ef669d53
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-05-17 15:52:50 +0530

    deletion in linked list

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -55,6 +55,30 @@ bool search(node* head, int key) {
     return false;
 }
 
+void deleteAtHead(node* &head) {
+    node* todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
+void deletion(node* &head, int val) {
+    if(head == NULL) {
+        return;
+    }
+    if(head->next == NULL) {
+        deleteAtHead(head);
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next->data != val) {
+        temp = temp->next;
+    }
+    node* todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
 int main() {
     node* head = NULL;
 
@@ -68,6 +92,10 @@ int main() {
     display(head);
     cout<<search(head, 5)<<endl;
     cout<<search(head, 7)<<endl;
+    deletion(head, 3);
+    display(head);
+    deleteAtHead(head);
+    display(head);
 
     return 0;
 }
