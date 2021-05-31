4f7f6e60ecf7c2516da3500b6aa7ee82971d0c0a
Chaitanya Singh Bisht <csbwork18@gmail.com>
2021-05-31 15:35:57 +0530

    doubly

--- ReverseLL.cpp
+++ ReverseLL.cpp
@@ -12,12 +12,6 @@ public:
     }
 };
 
-void insertAtHead(node* &head, int val) {
-    node* n = new node(val);
-    n->next = head;
-    head = n;
-}
-
 void insertAtTail(node* &head, int val) {
     node* n = new node(val);
 
@@ -35,6 +29,7 @@ void insertAtTail(node* &head, int val) {
 
 }
 
+
 void display(node* head) {
     node* temp = head;
     while(temp != NULL) {
@@ -42,41 +37,6 @@ void display(node* head) {
         temp = temp->next;
     }
     cout<<"NULL"<<endl;
-}
-
-bool search(node* head, int key) {
-    node* temp = head;
-    while(temp != NULL) {
-        if(temp->data == key) {
-            return true;
-        }
-        temp = temp->next;
-    }
-    return false;
-}
-
-void deleteAtHead(node* &head) {
-    node* todelete = head;
-    head = head->next;
-    delete todelete;
-}
-
-void deletion(node* &head, int val) {
-    if(head == NULL) {
-        return;
-    }
-    if(head->next == NULL) {
-        deleteAtHead(head);
-        return;
-    }
-
-    node* temp = head;
-    while(temp->next->data != val) {
-        temp = temp->next;
-    }
-    node* todelete = temp->next;
-    temp->next = temp->next->next;
-    delete todelete;
 }
 
 node* reverse(node* &head) {    //Iterative way of reversing a linked list
@@ -116,17 +76,9 @@ int main() {
     insertAtTail(head, 4);
     insertAtTail(head, 5);
     display(head);
-    insertAtHead(head, 0);
-    display(head);
-    cout<<search(head, 5)<<endl;
-    cout<<search(head, 7)<<endl;
-    deletion(head, 3);
-    display(head);
-    deleteAtHead(head);
-    display(head);
 
-    node* newhead = reverseRecursive(head);  //Iterative way of reversing a linked list
-    display(newhead);
+
+    node* newhead = reverseRecursive(head);
 
     return 0;
 }
